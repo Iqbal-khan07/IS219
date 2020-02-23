edc08bf3f8aff4673fa4717fa99aae28bb196f3a
Zoraiz Naeem <zoraiznaeem9@gmail.com>
2020-02-23 01:45:08 -0500

    changed Node Version in yml

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Calculator = require("./utils/Calculator")
 const calc = new Calculator();
 try {
     console.log(calc.add('2', 2))
+    console.log(calc.add(2, 2));
 } catch (error) {
     console.error(error)
 }
