a14e6ed8bb72d993882ca950ae8d755b5b797faa
Zoraiz Naeem <zoraiznaeem9@gmail.com>
2020-03-09 14:50:37 -0400

    fixed path in the CSV tests

--- test/CSVReader.test.js
+++ test/CSVReader.test.js
@@ -1,23 +1,28 @@
 const CSVReader = require("../utils/CSVReader")
 const City = require("../models/City")
+const path = require("path")
+
+var appdir = path.dirname("index.js")
+const dataFilePath = `${appdir}/test/testData/testCitiesData.csv`
+
 
 test("Create a CSVReader Object", () => {
     let csv = new CSVReader("./testData/testCitiesData.csv", City)
     expect(csv).toBeInstanceOf(CSVReader)
 })
 
-// test("Cheaking if correct instance of records", async () => {
-//     let reader = new CSVReader("./testData/testCitiesData.csv", City);
-//     const {output} = await reader.parse()
-//     expect(output[0]).toBeInstanceOf(City)
-// })
+test("Cheaking if correct instance of records", async () => {
+    let reader = new CSVReader(dataFilePath, City);
+    const {output} = await reader.parse()
+    expect(output[0]).toBeInstanceOf(City)
+})
 
-// test("Checking Number of Records of CSVReader", () => {
-//     let csv = new CSVReader("./testData/testCitiesData.csv", City)
-//     csv.parse().then(({size}) => {
-//         expect(size).toBe(2)
-//     }).catch(error => {
-//         console.error(error)
-//     })
-// })
+test("Checking Number of Records of CSVReader", () => {
+    let csv = new CSVReader(dataFilePath, City)
+    csv.parse().then(({size}) => {
+        expect(size).toBe(2)
+    }).catch(error => {
+        console.error(error)
+    })
+})
 
