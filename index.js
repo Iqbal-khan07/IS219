56a98e0698823e80a347ad7b0a7beeea5acd48a4
Zoraiz Naeem <zoraiznaeem9@gmail.com>
2020-03-14 18:52:46 -0400

    updated index file

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const CSVReader = require("./utils/CSVReader")
-const City = require("./models/City")
+// const CSVReader = require("./utils/CSVReader")
+// const City = require("./models/City")
 
 
-let reader = new CSVReader("./test/testData/testCitiesData.csv", City);
+// let reader = new CSVReader("./test/testData/testCitiesData.csv", City);
 
-const test = async () => {
-    try {
-        let {output, size} = await reader.parse();
-        let record = output[0]  
-        console.log(record)  
-    } catch (error) {
-        console.log(error)
-    }
-}
+// const test = async () => {
+//     try {
+//         let {output, size} = await reader.parse();
+//         let record = output[0]  
+//         console.log(record)  
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 
-test();
+// test();
+// const Random = require("./utils/Random/Random")
+
+// let output = Random.nRandomElementsFromArray([1, 2, 3, 4, 5], 10, 'apple')
+// console.log(output)
+
+// Random.randomElementFromArray([], 1)
+
+const StatisticsCalculator = require("./utils/StatisticsCalculator/StatisticsCalculator")
+
+const calculator = new StatisticsCalculator()
+console.log(calculator.mean([1, 2, 3, 4]))
+
