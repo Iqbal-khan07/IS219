3ce9a9c4fc47a9695a88935509a3914032a9f142
Zoraiz Naeem <zoraiznaeem9@gmail.com>
2020-03-15 18:25:26 -0400

    added TestArray function

--- utils/arithUtil.js
+++ utils/arithUtil.js
@@ -7,6 +7,10 @@ const isArray = (arr) => {
     return Array.isArray(arr);
 }
 
+const isEmpty = (arr) => {
+    return arr.length === 0
+}
+
 const isNumArray = (arr) => {
     if(!Array.isArray(arr)) return false
     let isArray = true
@@ -19,9 +23,16 @@ const isNumArray = (arr) => {
     return isArray
 }
 
+const NumericArrayTests = (array) => {
+    if(isEmpty(array)) throw new Error("Empty Array") 
+    if(!isNumArray(array)) throw new Error("Input Must be a Integer array")
+}
+
 
 module.exports = {
     isNumArray,
     isNumber,
-    isArray
+    isArray,
+    NumericArrayTests,
+    isEmpty
 }
